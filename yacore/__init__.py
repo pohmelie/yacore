aa2b794f0da725617fb9f5c1e04356581bd11819
Nikita Melentev <multisosnooley@gmail.com>
2023-07-24 22:51:06 +0400

    add top-level imports

--- yacore/__init__.py
+++ yacore/__init__.py
@@ -1,4 +1,10 @@
+# ruff: noqa
 from importlib import resources
+
+from cock import build_entrypoint, get_options_defaults, Config, Option
+from facet import ServiceMixin
+
+from yacore.injector import inject, register
 
 __version__ = resources.read_text("yacore", "version.txt").strip()
 version = tuple(map(int, __version__.split(".")))
